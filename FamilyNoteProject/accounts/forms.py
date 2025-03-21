9d8573029a5f4dfb467dab9235a37258b04c2192
“yokoo-mnk” <minako331@gmail.com>
2025-03-22 08:31:08 +0900

    acconutsのforms.pyを作成

---
+++ FamilyNoteProject/accounts/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import User
+from django.contrib.auth.password_validation import validate_password
+
+
+class RegistForm(forms.ModelForm):
+    
+    class Mete:
+        model = User
+        fields = ['username', 'email', 'password']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
+        labels = {
+            'username': '名前',
+            'email': 'メールアドレス',
+            'password': 'パスワード',
+        }
+        
+    def save(self, commit=False):
+        user = super().save(commit=False)
+        validate_password(self.cleaned_data['password'], user)
+        user.set_password(self.cleaned_data['password'])
+        user.save()
+        return user
