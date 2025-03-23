f3967c1bda4e1932452fbaf162f96f35c6cf8d5a
“yokoo-mnk” <minako331@gmail.com>
2025-03-23 23:08:03 +0900

    task_update.htmlの微調整

--- FamilyNoteProject/tasks/forms.py
+++ FamilyNoteProject/tasks/forms.py
@@ -2,8 +2,31 @@ from django import forms
 from .models import Task
 
 class TaskForm(forms.ModelForm):
+    due_date = forms.DateField(
+        label='日付・期限（必須）',
+        widget=forms.DateInput(attrs={'type': 'date'})
+    )
+    
+    time = forms.TimeField(
+        label='時間（任意）',
+        widget=forms.TimeInput(attrs={'type': 'time'})
+    )
+    
+    category = forms.ChoiceField(
+        choices=Task.CATEGORY_CHOICES,
+        label='カテゴリ（必須）'
+    )
+    
     class Meta:
         model = Task
         fields = [ 
             'category', 'due_date', 'time', 'title', 'memo', 'image'
         ]
+        labels = {
+            'category': 'カテゴリ（必須）',
+            'due_date': '日付（必須）',
+            'time': '時間（任意）',
+            'title': 'タイトル（必須）',
+            'memo': 'メモ（必須）',
+            'image': '写真',
+        }
--- FamilyNoteProject/tasks/views.py
+++ FamilyNoteProject/tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import (
-    CreateView,
+    CreateView, UpdateView
 )
 from .models import Task
 from .forms import TaskForm
@@ -12,9 +12,22 @@ class TaskListView(ListView):
     template_name = 'task_list.html'
     context_object_name = 'tasks'
     paginate_by = 10
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        for task in context['tasks']:
+            task.formatted_due_date = task.due_date.strftime('%m/%d')
+        return context
 
 class TaskCreateView(CreateView):
     model = Task
     form_class = TaskForm
     template_name = 'task_create.html'
+    success_url = reverse_lazy('task_list')
+    
+class TaskUpdateView(UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'task_update.html'
     success_url = reverse_lazy('task_list')
