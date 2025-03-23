a930e809021c4863b919672508a0119d7dbdde06
“yokoo-mnk” <minako331@gmail.com>
2025-03-23 18:49:54 +0900

    タスク登録画面作成（cssまだ）

--- FamilyNoteProject/tasks/urls.py
+++ FamilyNoteProject/tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import (
-    TaskListView,
+    TaskListView, TaskCreateView,
 )
 
 app_name = 'tasks'
 urlpatterns = [
     path('task_list/', TaskListView.as_view(template_name='tasks/task_list.html'), name='task_list'),
+    path('task_create/', TaskCreateView.as_view(template_name='tasks/task_create.html'), name='task_create'),
 ]
--- FamilyNoteProject/tasks/views.py
+++ FamilyNoteProject/tasks/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic.list import ListView
+from django.views.generic.edit import (
+    CreateView,
+)
 from .models import Task
+from .forms import TaskForm
+from django.urls import reverse_lazy
 
 class TaskListView(ListView):
     model = Task
-    template_name = 'task_list.html'  # 使用するテンプレートのファイル名
-    context_object_name = 'tasks'  # テンプレートで利用するコンテキスト変数名
-    paginate_by = 10 
+    template_name = 'task_list.html'
+    context_object_name = 'tasks'
+    paginate_by = 10
+
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'task_create.html'
+    success_url = reverse_lazy('task_list')
