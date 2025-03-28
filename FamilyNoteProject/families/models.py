3d7045d937432fec850aa29abee5580689b75940
“yokoo-mnk” <minako331@gmail.com>
2025-03-29 03:09:20 +0900

    families models.py作成

--- FamilyNoteProject/families/models.py
+++ FamilyNoteProject/families/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Family(models.Model):
+    name = models.CharField(max_length=100)
+    invite_code = models.UUIDField(default=uuid.uuid4, unique=True)
+
+    def __str__(self):
+        return self.name
