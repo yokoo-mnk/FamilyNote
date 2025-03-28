5007384917c8a183e6dd22e656c68b24ab05d2db
“yokoo-mnk” <minako331@gmail.com>
2025-03-28 22:49:17 +0900

    new ユーザー登録views.py作成

--- FamilyNoteProject/accounts/views.py
+++ FamilyNoteProject/accounts/views.py
@@ -1,3 +1,27 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
+
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("profile")
+    
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "accounts/register.html", {"form": form})
+
+
+
+
+
+
+
+
+
 # from django.shortcuts import render, redirect, get_object_or_404
 # from django.views.generic import(
 #     FormView, View, DetailView,
