813673263218144e73cbcd7108f679c5a829d97b
“yokoo-mnk” <minako331@gmail.com>
2025-03-29 03:13:26 +0900

    families views.py作成

--- FamilyNoteProject/families/views.py
+++ FamilyNoteProject/families/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Family
+from accounts.models import CustomUser
 
-# Create your views here.
+
+@login_required
+def create_family(request):
+    if request.method == "POST":
+        family_name = request.POST.get("family_name")
+        family = Family.objects.create(name=family_name)
+        request.user.family = family
+        request.user.save()
+        return redirect("mypage")
+    return render(request, "families/create_family.html")
+
+
+@login_required
+def invite_family(request):
+    if not request.user.family:
+        return redirect("create_family")
+    return render(request, "families/invite_family.html", {"invite_code": request.user.family.invite_code})
+
+
+@login_required
+def join_family(request, invite_code):
+    family = get_object_or_404(Family, invite_code=invite_code)
+    request.user.family = family
+    request.user.save()
+    return redirect("mypage")
