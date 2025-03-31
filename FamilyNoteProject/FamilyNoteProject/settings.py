0592c3f3c2acd64f4882045be3bf72e60d4d43bc
“yokoo-mnk” <minako331@gmail.com>
2025-03-31 11:06:47 +0900

    templates/cssのフォルダ位置修正

--- FamilyNoteProject/FamilyNoteProject/settings.py
+++ FamilyNoteProject/FamilyNoteProject/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'accounts/static', 'tasks/static', 'families/static'
 ]
 
 # Default primary key field type
