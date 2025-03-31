40872aace9c9dfae2334e5f653bf70540e586f22
“yokoo-mnk” <minako331@gmail.com>
2025-03-31 13:52:42 +0900

    タスク一覧・登録css作成

--- FamilyNoteProject/tasks/forms.py
+++ FamilyNoteProject/tasks/forms.py
@@ -9,7 +9,12 @@ class TaskForm(forms.ModelForm):
     
     start_time = forms.TimeField(
         label='時間（任意）',
+        required=False,
         widget=forms.TimeInput(attrs={'type': 'time'})
+    )
+    memo = forms.CharField(
+        label='メモ（任意）',
+        required=False,
     )
     
     category = forms.ChoiceField(
@@ -27,9 +32,15 @@ class TaskForm(forms.ModelForm):
             'due_date': '日付（必須）',
             'start_time': '時間（任意）',
             'title': 'タイトル（必須）',
-            'memo': 'メモ（必須）',
-            'image': '写真',
+            'memo': 'メモ（任意）',
+            'image': '写真（任意）',
         }
         widgets = {
             "is_favorite": forms.CheckboxInput(attrs={"class": "form-check-input"}),
         }
+    
+    def clean_start_time(self):
+        start_time = self.cleaned_data.get('start_time')
+        if start_time is None or start_time == '':
+            return None  # 空欄の場合は None に設定
+        return start_time
