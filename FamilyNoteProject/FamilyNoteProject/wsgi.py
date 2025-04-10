0f33dd17b0482f2fc10fdb21d9eb97a83858e307
“yokoo-mnk” <minako331@gmail.com>
2025-04-10 21:19:37 +0900

    Fix wsgi.py to use correct project name

--- FamilyNoteProject/FamilyNoteProject/wsgi.py
+++ FamilyNoteProject/FamilyNoteProject/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.append('/home/minakoyokoo/FamilyNote/FamilyNoteProject')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'FamilyNoteProject.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FamilyNoteProject.settings')
-
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FamilyNoteProject.settings')
 application = get_wsgi_application()
