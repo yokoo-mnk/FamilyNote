fe1a8af56b8d505e753ec06c516d03de22b39ecc
“yokoo-mnk” <minako331@gmail.com>
2025-04-11 16:08:18 +0900

    管理サイト作成

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,8 +2,27 @@ from django.contrib import admin
 from .models import CustomUser, Child
 from django.contrib.auth.admin import UserAdmin
 
-@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    ordering = ['email']
+    model = CustomUser
+
+    list_display = ('email', 'full_name', 'nickname', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('プロフィール情報', {'fields': ('full_name', 'nickname', 'image', 'family')}),
+        ('権限', {'fields': ('is_staff', 'is_active', 'groups', 'user_permissions')}),
+        ('日付情報', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'nickname', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+
+    search_fields = ('email', 'nickname')
+    ordering = ('email',)
     
-admin.site.register(Child)
+admin.site.register(CustomUser, CustomUserAdmin)
