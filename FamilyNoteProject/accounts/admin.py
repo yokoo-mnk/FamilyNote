4f346572ce30bfda4453730f4ae1a9f0dd0581a3
“yokoo-mnk” <minako331@gmail.com>
2025-04-11 10:41:35 +0900

    Clean up project structure

--- FamilyNoteProject/accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser, Child
-from django.contrib.auth.admin import UserAdmin
-
-@admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
-    ordering = ['email']
-    
-admin.site.register(Child)
